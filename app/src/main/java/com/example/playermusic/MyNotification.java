8ad5f208a97f11fa568822a60678ece453289f8d
mrcarromesa <qwe123456>
2019-12-15 19:20:58 -0300

    Prioridade da notificação.

--- app/src/main/java/com/example/playermusic/MyNotification.java
+++ app/src/main/java/com/example/playermusic/MyNotification.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.opengl.Visibility;
 import android.widget.RemoteViews;
 
 import androidx.core.app.NotificationCompat;
@@ -32,6 +33,9 @@ public class MyNotification extends Notification {
         notification.when=when;
         notification.tickerText=tickerText;
         notification.icon=R.mipmap.ic_launcher;
+        notification.visibility = VISIBILITY_PUBLIC;
+        notification.priority = PRIORITY_MAX;
+
 
         setContentViewToNotfication();
 
